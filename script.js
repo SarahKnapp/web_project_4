3da43ce258ae03576e1127b6818f881e8c1ea3f5
Sarah Knapp <knapp.sarah.l@gmail.com>
2020-11-22 13:25:30 -0500

    final edits

--- script.js
+++ script.js
@@ -9,14 +9,6 @@ let popupButton = document.querySelector(".popup__button");
 let closeButton = document.querySelector(".popup__close-icon");
 let form = document.querySelector(".popup__info");
 
-/*Declare your functions, you'll need three of them: to open the popup
- (it should also fill the form fields with the data from profile),
- to close the popup, and the last one to handle the submit action:
- modify the profile fields. */
-
- /*#1  open the popup
- (it should also fill the form fields with the data from profile),  */
-
 function openPopup() {
   popupState.classList.add("popup_opened");
   popupName.value = profileName.textContent;
@@ -27,13 +19,14 @@ function closePopup() {
   popupState.classList.remove("popup_opened");
 };
 
-function savePopup() {
-profileName.textContent = popupName.value;
-profileJob.textContent = popupJob.value;
-popupState.classList.remove("popup_opened");
+function savePopup(event) {
+  event.preventDefault();
+  profileName.textContent = popupName.value;
+  profileJob.textContent = popupJob.value;
+  popupState.classList.remove("popup_opened");
 };
 
 
 profileEdit.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
-popupButton.addEventListener("submit", savePopup);
+form.addEventListener("submit", savePopup);
