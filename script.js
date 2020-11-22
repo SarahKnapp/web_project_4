4fe9b742385d4e5ab7a81e1aab04db735a389846
Sarah Knapp <knapp.sarah.l@gmail.com>
2020-11-22 09:52:01 -0500

    Review edits

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 let profileDescription = document.querySelector(".profile__description");
 let profileName = document.querySelector(".profile__name");
-let popupName = document.querySelector(".popup__text_type_name");
 let profileJob = document.querySelector(".profile__job");
 let popupState = document.querySelector(".popup");
+let popupName = document.querySelector(".popup__text_type_name");
+let popupJob = document.querySelector(".popup__text_type_job");
+let profileEdit = document.querySelector(".profile__edit");
+let popupButton = document.querySelector(".popup__button");
+let closeButton = document.querySelector(".popup__close-icon");
+let form = document.querySelector(".popup__info");
 
-document.querySelector(".profile__edit").addEventListener("click", function () {
+/*Declare your functions, you'll need three of them: to open the popup
+ (it should also fill the form fields with the data from profile),
+ to close the popup, and the last one to handle the submit action:
+ modify the profile fields. */
+
+ /*#1  open the popup
+ (it should also fill the form fields with the data from profile),  */
+
+function openPopup() {
   popupState.classList.add("popup_opened");
-  let currentProfileName = document.querySelector(".profile__name").textContent;
-  document.querySelector(".popup__text_type_name").value = currentProfileName;
-  let z = document.querySelector(".profile__job").textContent;
-  document.querySelector(".popup__text_type_job").value = z;
-});
+  popupName.value = profileName.textContent;
+  popupJob.value = profileJob.textContent;
+};
 
-document.querySelector(".popup__button").addEventListener("click", function () {
-  let x = document.querySelector(".popup__text_type_name").value;
-  let y = document.querySelector(".popup__text_type_job").value;
-  profileName.innerHTML = x;
-  profileJob.innerHTML = y;
+function closePopup() {
   popupState.classList.remove("popup_opened");
-});
+};
 
-document.querySelector(".popup__close-icon").addEventListener("click", function () {
-  popupState.classList.remove("popup_opened");
-});
+function savePopup() {
+profileName.textContent = popupName.value;
+profileJob.textContent = popupJob.value;
+popupState.classList.remove("popup_opened");
+};
+
+
+profileEdit.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+popupButton.addEventListener("submit", savePopup);
